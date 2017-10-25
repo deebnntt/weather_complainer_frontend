a308e16f3dc3f8fbb555a5ed5e326b6ce3948c4d
deebnntt <daniellebennettchan@gmail.com>
2017-10-25 10:15:24 -0400

    styling

--- app.js
+++ app.js
@@ -6,23 +6,28 @@ function complexAlgorithm(historic, current) {
 	let complaintArr = [];
 	for (var key in current) current = current[key];
 	let avgHigh = historic.valueOf().daily_avg_high;
-	if (current.precipitation > 0) {
-		complaintArr.push("It's raining!");
+	if (current.precipitation > 0.15) {
+		complaintArr.push("It's going to rain!");
 	}
 	if (current.high - historic.daily_avg_high >= 10) {
 		complaintArr.push("It's unseasonably hot!");
 	}
-	if (current.low - historic.daily_avg_low <= -10) {
+	if (historic.daily_avg_low - current.low >= 10) {
 		complaintArr.push("It's going to be so cold tonight");
 	}
-	if (current.high - historic.daily_avg_high <= 10) {
+	if (historic.daily_avg_high - current.high >= 10) {
 		complaintArr.push("It's going to be unseasonably cold today");
 	}
 	if (
 		current.high - historic.daily_avg_high >= 10 &&
-		current.precipitation > 0
+		current.precipitation > 0.05
 	) {
 		complaintArr.push("It's so muggy and gross!");
+	}
+	if (complaintArr.length === 0) {
+		complaintArr.push(
+			'I have nothing to complain about, but I still feel sad.'
+		);
 	}
 	renderComplaints(complaintArr);
 }
